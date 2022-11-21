42f95af00d232f2f39b72d8c9728cef7ac9a39fc
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-21 16:54:07 -0300

    Subindo a aplicação e registrando o microservice no Eureka Server

---
+++ msclientes/src/main/java/io/github/luaprogrammer/msclientes/MsclientesApplication.java
@@ -0,0 +1,13 @@
+package io.github.luaprogrammer.msclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MsclientesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsclientesApplication.class, args);
+    }
+}
