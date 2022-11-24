82e08fea655a7c2fa95393e7b7bd43b8592a05a3
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-24 13:00:18 -0300

    Buscando os dadosde cartões pelo microservice de cartões

--- msavaliadorcredito/src/main/java/io/github/luaprogrammer/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/luaprogrammer/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,24 +1,33 @@
 package io.github.luaprogrammer.msavaliadorcredito.application;
 
+import io.github.luaprogrammer.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.luaprogrammer.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.luaprogrammer.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.luaprogrammer.msavaliadorcredito.infra.CartaoResourceClient;
 import io.github.luaprogrammer.msavaliadorcredito.infra.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient client;
+    private final CartaoResourceClient cartao;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
-        //obter dados do msclientes e obter cartoes do cliente mscartoes
 
         ResponseEntity<DadosCliente> dadosClienteResponse = client.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartao.getCartoesByCliente(cpf);
 
-        return SituacaoCliente.builder().cliente(dadosClienteResponse.getBody()).build();
+        return SituacaoCliente
+                .builder()
+                .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
+                .build();
     }
 
 }
---
+++ msavaliadorcredito/src/main/java/io/github/luaprogrammer/msavaliadorcredito/infra/CartaoResourceClient.java
@@ -0,0 +1,17 @@
+package io.github.luaprogrammer.msavaliadorcredito.infra;
+
+import io.github.luaprogrammer.msavaliadorcredito.domain.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+//url = "http://localhost:8080"
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartaoResourceClient {
+
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesByCliente(@RequestParam("cpf") String cpf);
+}
